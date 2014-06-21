34dbc8f2bd0ca3988d48455c0ba7ef51ac88a83b
Marco Agnese <ma2413@imperial.ac.uk>
2014-06-21 17:44:20 +0100

    threadstest: modified (does not work)

--- dune/common/parallel/test/threadstest.cc
+++ dune/common/parallel/test/threadstest.cc
@@ -4,122 +4,41 @@
 
 #include <iostream>
 #include <vector>
+#include <thread>
 
-#include <dune/common/enumset.hh>
+#define NUM_THREADS 2
 
-#include <dune/common/parallel/mpicollectivecommunication.hh>
-#include <dune/common/parallel/indexset.hh>
-#include <dune/common/parallel/plocalindex.hh>
-#include <dune/common/parallel/remoteindices.hh>
-#include <dune/common/parallel/interface.hh>
-#include <dune/common/parallel/communicator.hh>
+void init_a(int tid,std::vector<int>& a){
 
-// policy: copy
-template<typename T>
-class CopyData{
+    if(tid==0){
+        for(int i=0;i!=6;++i) a[i]=i;
+    }
 
-public:
+    if(tid==1){
+        for(int i=6;i!=12;++i) a[i]=i;
+    }
 
-    typedef typename T::value_type IndexedType;
-
-    static IndexedType gather(const T& v,int i){return v[i];}
-    static void scatter(T& v,IndexedType item,int i){v[i]=item;}
-
-};
-
-// policy: add
-template<typename T>
-class AddData{
-
-public:
-
-    typedef typename T::value_type IndexedType;
-
-    static IndexedType gather(const T& v,int i){return v[i];}
-    static void scatter(T& v,IndexedType item,int i){v[i]+=item;}
-
-};
-
+}
 
 int main(int argc,char** argv){
 
-    #if HAVE_MPI
+    // create global vector a
+    std::vector<int> a(12,0);
 
-    // init MPI
-    MPI_Init(&argc,&argv);
-    Dune::CollectiveCommunication<MPI_Comm> collCom(MPI_COMM_WORLD);
+    //launch a group of threads to fill the global vector
+    std::thread t[NUM_THREADS];
 
-    // get size and rank
-    int nProcs(collCom.size());
-    int rank(collCom.rank());
-
-    // 2 processes
-    if(nProcs>2){
-        if(rank==0) std::cout<<std::endl<<"WARNING: run me again with just 2 processes"<<std::endl;
+    for(int i=0;i!=NUM_THREADS;++i){
+        t[i]=std::thread(init_a,i,a);
     }
-    else{
 
-        // define parallel local index and parallel index set
-        enum flags{owner,ghost};
-        typedef Dune::ParallelLocalIndex<flags> LocalIndexType;
-        typedef Dune::ParallelIndexSet<int,LocalIndexType,7> ParallelIndexType;
+    for(int i=0;i!=NUM_THREADS;++i) t[i].join();
 
-        // create parallel index set s
-        ParallelIndexType sis;
-        int offsetGlobal(0);
-        int offsetLocal(0);
-
-        if(rank==1){
-            offsetGlobal=6;
-            offsetLocal=1;
-        }
-
-        sis.beginResize();
-        for(int i=0;i!=6;++i) sis.add(i+offsetGlobal,LocalIndexType(i+offsetLocal,owner));
-        if(rank==0) sis.add(6,LocalIndexType(6,ghost));
-        else sis.add(5,LocalIndexType(0,ghost));
-        sis.endResize();
-
-        // output sis
-        for(int i=0;i!=nProcs;++i){
-            if(rank==i){
-                std::cout<<"s"<<rank<<":"<<std::endl;
-                std::cout<<sis<<std::endl<<std::endl;
-            }
-            collCom.barrier();
-        }
-
-        // set remote indices
-        typedef Dune::RemoteIndices<ParallelIndexType> RemoteIndicesType;
-        RemoteIndicesType riS(sis,sis,MPI_COMM_WORLD);
-        riS.rebuild<true>();
-
-        // create interface
-        Dune::EnumItem<flags,ghost> ghostFlags;
-        Dune::EnumItem<flags,owner> ownerFlags;
-
-        typedef Dune::Interface InterfaceType;
-        InterfaceType infS(MPI_COMM_WORLD);
-        infS.build(riS,ownerFlags,ghostFlags);
-
-        // create local vector al
-        std::vector<int> al(7,0);
-
-        typedef typename ParallelIndexType::iterator PIndexIterType;
-        for(PIndexIterType it=sis.begin();it!=sis.end();++it){
-            if(it->local().attribute()==owner) al[it->local().local()]=(it-sis.begin())+5*rank;
-        }
-
-        // output al
-        for(int i=0;i!=nProcs;++i){
-            if(rank==i){
-                std::cout<<"Local vector on process "<<rank<<": al={ ";
-                for(std::vector<int>::iterator it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
-                std::cout<<"}"<<std::endl;
-            }
-            collCom.barrier();
-        }
-
+    // output global vector
+    std::cout<<"Global vector on main thread: a={ ";
+    for(std::vector<int>::iterator it=a.begin();it!=a.end();++it) std::cout<<*it<<" ";
+    std::cout<<"}"<<std::endl;
+/*
         // do something on al
         if(rank==0) std::cout<<std::endl<<"Performing the operation al[i]+=10*(rank+1) for only the owned entries"<<std::endl<<std::endl;
         for(PIndexIterType it=sis.begin();it!=sis.end();++it){
@@ -161,7 +80,7 @@ int main(int argc,char** argv){
     MPI_Finalize();
 
     #endif //HAVE_MPI
-
+*/
     return 0;
 
 }
