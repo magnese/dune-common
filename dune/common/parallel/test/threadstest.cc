8965b98e1777fbbf6821e088aa1559c660773c37
Marco Agnese <ma2413@imperial.ac.uk>
2014-06-23 13:50:24 +0100

    threadstest: completed

--- dune/common/parallel/test/threadstest.cc
+++ dune/common/parallel/test/threadstest.cc
@@ -4,19 +4,61 @@
 
 #include <iostream>
 #include <vector>
+
 #include <thread>
+#include <mutex>
 
 #define NUM_THREADS 2
 
+std::mutex mtx;
+
 void init_a(int tid,std::vector<int>& a){
 
-    if(tid==0){
-        for(int i=0;i!=6;++i) a[i]=i;
-    }
+    // start of the critical section
+    mtx.lock();
 
-    if(tid==1){
-        for(int i=6;i!=12;++i) a[i]=i;
-    }
+    int offset(6*tid);
+    for(int i=offset;i!=(6+offset);++i) a[i]=i;
+
+    // output global vector
+    std::cout<<"Global vector after the call of init_a from thread "<<tid<<": a={ ";
+    for(std::vector<int>::iterator it=a.begin();it!=a.end();++it) std::cout<<*it<<" ";
+    std::cout<<"}"<<std::endl;
+
+    // end of the critical section
+    mtx.unlock();
+
+}
+
+void init_a_optimized(int tid,std::vector<int>& a){
+
+    int offset(6*tid);
+    for(int i=offset;i!=(6+offset);++i) a[i]=i;
+
+    // start of the critical section
+    mtx.lock();
+
+    // output global vector
+    std::cout<<"Global vector after the call of init_a_optimized from thread "<<tid<<": a={ ";
+    for(std::vector<int>::iterator it=a.begin();it!=a.end();++it) std::cout<<*it<<" ";
+    std::cout<<"}"<<std::endl;
+
+    // end of the critical section
+    mtx.unlock();
+
+}
+
+void clear_a(int tid,std::vector<int>& a){
+
+    int offset(6*tid);
+    for(int i=offset;i!=(6+offset);++i) a[i]=0;
+
+}
+
+void oper_a(int tid,std::vector<int>& a){
+
+    int offset(6*tid);
+    for(int i=offset;i!=(6+offset);++i) a[i]+=10*(tid+1);
 
 }
 
@@ -25,62 +67,41 @@ int main(int argc,char** argv){
     // create global vector a
     std::vector<int> a(12,0);
 
-    //launch a group of threads to fill the global vector
-    std::thread t[NUM_THREADS];
+    // launch a group of threads to fill the global vector a with init_a
+    std::vector<std::thread> t(NUM_THREADS);
 
-    for(int i=0;i!=NUM_THREADS;++i){
-        t[i]=std::thread(init_a,i,a);
-    }
-
+    for(int i=0;i!=NUM_THREADS;++i) t[i]=std::thread(init_a,i,std::ref(a));
     for(int i=0;i!=NUM_THREADS;++i) t[i].join();
 
-    // output global vector
+    // output global vector a
     std::cout<<"Global vector on main thread: a={ ";
     for(std::vector<int>::iterator it=a.begin();it!=a.end();++it) std::cout<<*it<<" ";
-    std::cout<<"}"<<std::endl;
-/*
-        // do something on al
-        if(rank==0) std::cout<<std::endl<<"Performing the operation al[i]+=10*(rank+1) for only the owned entries"<<std::endl<<std::endl;
-        for(PIndexIterType it=sis.begin();it!=sis.end();++it){
-            if(it->local().attribute()==owner) al[it->local().local()]+=10*(rank+1);
-        }
+    std::cout<<"}"<<std::endl<<std::endl;;
 
-        // output al before communication
-        for(int i=0;i!=nProcs;++i){
-            if(rank==i){
-                std::cout<<"Local vector on process "<<rank<<": al={ ";
-                for(std::vector<int>::iterator it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
-                std::cout<<"}"<<std::endl;
-            }
-            collCom.barrier();
-        }
+    // clear global vector a
+    std::cout<<"Clearing the global vector"<<std::endl<<std::endl;
+    for(int i=0;i!=NUM_THREADS;++i) t[i]=std::thread(clear_a,i,std::ref(a));
+    for(int i=0;i!=NUM_THREADS;++i) t[i].join();
 
-        // create communicator
-        typedef Dune::BufferedCommunicator CommunicatorType;
-        CommunicatorType bComm;
-        bComm.build(al,al,infS);
+    // launch a group of threads to fill the global vector a with init_a_optimized
+    for(int i=0;i!=NUM_THREADS;++i) t[i]=std::thread(init_a_optimized,i,std::ref(a));
+    for(int i=0;i!=NUM_THREADS;++i) t[i].join();
 
-        // communicate
-        if(rank==0) std::cout<<std::endl<<"Forward communication"<<std::endl<<std::endl;
-        bComm.forward<CopyData<std::vector<int> > >(al,al);
+    // output global vector a
+    std::cout<<"Global vector on main thread: a={ ";
+    for(std::vector<int>::iterator it=a.begin();it!=a.end();++it) std::cout<<*it<<" ";
+    std::cout<<"}"<<std::endl<<std::endl;
 
-        // output al after communication
-        for(int i=0;i!=nProcs;++i){
-            if(rank==i){
-                std::cout<<"Local vector on process "<<rank<<": al={ ";
-                for(std::vector<int>::iterator it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
-                std::cout<<"}"<<std::endl;
-            }
-            collCom.barrier();
-        }
+    // do something on global vector a
+    std::cout<<"Performing the operation a[i]+=10*(thread_ID+1)"<<std::endl;
+    for(int i=0;i!=NUM_THREADS;++i) t[i]=std::thread(oper_a,i,std::ref(a));
+    for(int i=0;i!=NUM_THREADS;++i) t[i].join();
 
-    }
+    // output global vector a
+    std::cout<<"Global vector on main thread: a={ ";
+    for(std::vector<int>::iterator it=a.begin();it!=a.end();++it) std::cout<<*it<<" ";
+    std::cout<<"}"<<std::endl<<std::endl;
 
-    // finalize MPI
-    MPI_Finalize();
-
-    #endif //HAVE_MPI
-*/
     return 0;
 
 }
