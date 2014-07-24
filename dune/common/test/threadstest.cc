ab56fc58adaeb7e6271e8d31ac4e510786b950d7
Marco Agnese <ma2413@imperial.ac.uk>
2014-07-24 13:29:53 +0100

    threadstest: cleanup

--- dune/common/test/threadstest.cc
+++ dune/common/test/threadstest.cc
@@ -26,7 +26,7 @@ double ux1(2.0);
 // threads control
 #define NUM_THREADS 4
 #define GRID_ELEMENTS_PER_THREAD 2
-#define DEBUG_THREADS_TEST 1
+#define DEBUG_FLAG 1
 
 // basis functions
 inline double phi0(double&& x){return 1.0-x;}
@@ -41,191 +41,191 @@ enum flags{shared,NOTshared};
 template<typename AType,typename bType,typename IType,typename GType,typename FType>
 void assemble(unsigned int tid,AType& A,bType& b,IType& idx,GType& grid,unsigned int numGridElements,FType& phi,FType& derphi){
 
-    std::vector<std::vector<double>> Alocal(2,std::vector<double>(2,0.0));
-    std::vector<double> blocal(2,0.0);
+  std::vector<std::vector<double>> Alocal(2,std::vector<double>(2,0.0));
+  std::vector<double> blocal(2,0.0);
 
-    size_t startElem(tid*numGridElements);
-    size_t endElem((tid+1)*numGridElements);
-    for(size_t elem=startElem;elem!=endElem;++elem){
+  size_t startElem(tid*numGridElements);
+  size_t endElem((tid+1)*numGridElements);
+  for(size_t elem=startElem;elem!=endElem;++elem){
 
-        // assemble local A and local b
-        for(size_t i=0;i!=2;++i){
+    // assemble local A and local b
+    for(size_t i=0;i!=2;++i){
 
-            blocal[i]=0.0;
-            // using trapezoid rule
-            blocal[i]+=0.5*(phi[i](0.0)*f(0.0));
-            blocal[i]+=0.5*(phi[i](1.0)*f(1.0));
-            blocal[i]*=(grid[elem+1]-grid[elem]);
+      blocal[i]=0.0;
+      // using trapezoid rule
+      blocal[i]+=0.5*(phi[i](0.0)*f(0.0));
+      blocal[i]+=0.5*(phi[i](1.0)*f(1.0));
+      blocal[i]*=(grid[elem+1]-grid[elem]);
 
-            for(size_t j=0;j!=2;++j){
-                Alocal[i][j]=0.0;
-                // using trapezoid rule
-                Alocal[i][j]+=0.5*(derphi[i](0.0)*derphi[j](0.0));
-                Alocal[i][j]+=0.5*(derphi[i](1.0)*derphi[j](1.0));
-                Alocal[i][j]/=(grid[elem+1]-grid[elem]);
-            }
-
-        }
-
-        // adding local A and local b to the global stiffness matrix and the global RHS
-        for(size_t i=0;i!=2;++i){
-            // possible critical section
-            if(idx[elem+i].first==shared) idx[elem+i].second->lock();
-            b[elem+i]+=blocal[i];
-            for(size_t j=0;j!=2;++j){
-                // possible critical section
-                if(idx[elem+j].first==shared) idx[elem+j].second->lock();
-                A[elem+i][elem+j]+=Alocal[i][j];
-                if(idx[elem+j].first==shared) idx[elem+j].second->unlock();
-            }
-            if(idx[elem+i].first==shared) idx[elem+i].second->unlock();
-        }
+      for(size_t j=0;j!=2;++j){
+        Alocal[i][j]=0.0;
+        // using trapezoid rule
+        Alocal[i][j]+=0.5*(derphi[i](0.0)*derphi[j](0.0));
+        Alocal[i][j]+=0.5*(derphi[i](1.0)*derphi[j](1.0));
+        Alocal[i][j]/=(grid[elem+1]-grid[elem]);
+      }
 
     }
 
+    // adding local A and local b to the global stiffness matrix and the global RHS
+    for(size_t i=0;i!=2;++i){
+      // possible critical section
+      if(idx[elem+i].first==shared) idx[elem+i].second->lock();
+      b[elem+i]+=blocal[i];
+      for(size_t j=0;j!=2;++j){
+        // possible critical section
+        if(idx[elem+j].first==shared) idx[elem+j].second->lock();
+        A[elem+i][elem+j]+=Alocal[i][j];
+        if(idx[elem+j].first==shared) idx[elem+j].second->unlock();
+      }
+      if(idx[elem+i].first==shared) idx[elem+i].second->unlock();
+    }
+
+  }
+
+}
+
+// print string
+void print(std::string&& str){
+  #ifdef DEBUG_FLAG
+  #if DEBUG_FLAG
+  std::cout<<"DEBUG: "<<str<<std::endl;
+  #endif
+  #endif
 }
 
 // print vector function
-template<typename VType>
-void printVector(VType& vector){
-    size_t size(vector.size());
-    for(size_t i=0;i!=size;++i) std::cout<<vector[i]<<" ";
-    std::cout<<std::endl<<std::endl;
+template<typename V>
+void printVector(V& v){
+  #ifdef DEBUG_FLAG
+  #if DEBUG_FLAG
+  for(typename V::iterator it=v.begin();it!=v.end();++it) std::cout<<*it<<" ";
+  std::cout<<std::endl;
+  #endif
+  #endif
 }
 
 // print matrix function
-template<typename MType>
-void printMatrix(MType& matrix){
-    size_t size(matrix.size());
-    for(size_t i=0;i!=size;++i){
-        for(size_t j=0;j!=size;++j) std::cout<<matrix[i][j]<<" ";
-        std::cout<<std::endl;
-    }
-    std::cout<<std::endl;
+template<typename M>
+void printMatrix(M& m){
+  #ifdef DEBUG_FLAG
+  #if DEBUG_FLAG
+  for(typename M::iterator rowIt=m.begin();rowIt!=m.end();++rowIt) printVector(*rowIt);
+  std::cout<<std::endl;
+  #endif
+  #endif
 }
 
-int main(void){
+int main(int argc, char** argv){
 
-    // initial description
-    std::cout<<"FEM scheme over "<<GRIDDIM<<"D grid in a "<<WORLDDIM<<"D world to solve equation -u''=f with Dirichlet boundary conditions u("<<COOR_X0<<")="<<ux0<<" and u("<<COOR_X1<<")="<<ux1<<"."<<std::endl<<std::endl;
+  // initial description
+  std::cout<<"FEM scheme over "<<GRIDDIM<<"D grid in a "<<WORLDDIM<<"D world to solve equation -u''=f with Dirichlet boundary conditions u("<<COOR_X0<<")="<<ux0<<" and u("<<COOR_X1<<")="<<ux1<<"."<<std::endl<<std::endl;
 
-    // geometry definition
-    typedef Dune::FieldVector<double,WORLDDIM> CoordType;
-    CoordType x0(COOR_X0);
-    CoordType x1(COOR_X1);
+  // geometry definition
+  typedef double ctype;
+  typedef Dune::FieldVector<ctype,WORLDDIM> CoordType;
+  CoordType x0(COOR_X0);
+  CoordType x1(COOR_X1);
 
-    if(x0[0]>x1[0]){
-        std::swap(x0[0],x1[0]);
-        std::swap(ux0,ux1);
-    }
+  if(x0[0]>x1[0]){
+    std::swap(x0[0],x1[0]);
+    std::swap(ux0,ux1);
+  }
 
-    std::cout<<"Coordinate of the starting point of the grid : "<<x0<<std::endl;
-    std::cout<<"Coordinate of the ending point of the grid : "<<x1<<std::cout<<std::endl;
+  std::cout<<"Coordinate of the starting point of the grid : "<<x0<<std::endl;
+  std::cout<<"Coordinate of the ending point of the grid : "<<x1<<std::cout<<std::endl;
 
-    // number of threads
-    unsigned int numThreads((NUM_THREADS<2?2:NUM_THREADS));
-    std::cout<<"Number of threads to use : "<<numThreads<<std::endl;
+  // number of threads
+  const size_t numThreads((NUM_THREADS<2?2:NUM_THREADS));
+  std::cout<<"Number of threads to use : "<<numThreads<<std::endl;
 
-    // number of grid elements managed by each thread
-    unsigned int numGridElementsPerThread((GRID_ELEMENTS_PER_THREAD<1?1:GRID_ELEMENTS_PER_THREAD));
-    std::cout<<"Number of grid elements for each thread : "<<numGridElementsPerThread<<std::endl<<std::endl;
+  // number of grid elements managed by each thread
+  const size_t numGridElementsPerThread((GRID_ELEMENTS_PER_THREAD<1?1:GRID_ELEMENTS_PER_THREAD));
+  std::cout<<"Number of grid elements for each thread : "<<numGridElementsPerThread<<std::endl<<std::endl;
 
-    // grid creation
-    unsigned int numNodes(numThreads*numGridElementsPerThread+1);
-    CoordType x1x0Vector(x1-x0);
-    double deltax(x1x0Vector.two_norm()/(numNodes-1));
-    typedef std::vector<CoordType> GridType;
-    GridType grid(numNodes,x0);
+  // grid creation
+  const size_t numNodes(numThreads*numGridElementsPerThread+1);
+  CoordType x1x0Vector(x1-x0);
+  ctype deltax(x1x0Vector.two_norm()/(numNodes-1));
+  typedef std::vector<CoordType> GridType;
+  GridType grid(numNodes,x0);
 
-    for(size_t i=1;i!=numNodes;++i) grid[i]+=(deltax*i);
+  for(size_t i=1;i!=numNodes;++i) grid[i]+=(deltax*i);
 
-    #ifdef DEBUG_THREADS_TEST
-    #if DEBUG_THREADS_TEST
-    std::cout<<"DEBUG: grid coordinates"<<std::endl;
-    for(std::vector<CoordType>::iterator it=grid.begin();it!=grid.end();++it) std::cout<<*it<<" ";
-    std::cout<<std::endl<<std::endl;
-    #endif
-    #endif
+  print("grid coordinates");
+  printVector(grid);
 
-    // set type of index (one mutex for each shared part of the vector)
-    typedef std::vector<std::pair<flags,std::recursive_mutex*>> ThreadsIndexType;
-    ThreadsIndexType tit(numNodes,std::pair<flags,std::recursive_mutex*>(NOTshared,nullptr));
+  // set type of index (one mutex for each shared part of the vector)
+  typedef std::vector<std::pair<flags,std::recursive_mutex*>> ThreadsIndexType;
+  ThreadsIndexType tit(numNodes,std::pair<flags,std::recursive_mutex*>(NOTshared,nullptr));
 
-    for(size_t i=0;i!=(numThreads-1);++i){
-        tit[(i+1)*numGridElementsPerThread].first=shared;
-        tit[(i+1)*numGridElementsPerThread].second=new std::recursive_mutex();
-    }
+  for(size_t i=0;i!=(numThreads-1);++i){
+    tit[(i+1)*numGridElementsPerThread].first=shared;
+    tit[(i+1)*numGridElementsPerThread].second=new std::recursive_mutex();
+  }
 
-    #ifdef DEBUG_THREADS_TEST
-    #if DEBUG_THREADS_TEST
-    std::cout<<"DEBUG: shared-NOTshared nodes"<<std::endl;
-    for(ThreadsIndexType::iterator it=tit.begin();it!=(tit.end()-1);++it){
-        if(it->first==NOTshared) std::cout<<"N--";
-        if(it->first==shared) std::cout<<"S--";
-    }
-    if((tit.rbegin())->first==NOTshared) std::cout<<"N"<<std::endl<<std::endl;
-    if((tit.rbegin())->first==shared) std::cout<<"S"<<std::endl<<std::endl;
-    #endif
-    #endif
+  print("shared-NOTshared nodes");
+  #ifdef DEBUG_FLAG
+  #if DEBUG_FLAG
+  for(ThreadsIndexType::iterator it=tit.begin();it!=(tit.end()-1);++it){
+    if(it->first==NOTshared) std::cout<<"N--";
+    if(it->first==shared) std::cout<<"S--";
+  }
+  if((tit.rbegin())->first==NOTshared) std::cout<<"N"<<std::endl<<std::endl;
+  if((tit.rbegin())->first==shared) std::cout<<"S"<<std::endl<<std::endl;
+  #endif
+  #endif
 
-    // allocate stiffness matrix A, RHS vector b and solution vector x
-    typedef Dune::DynamicMatrix<double> StiffnessMatrixType;
-    StiffnessMatrixType A(numNodes,numNodes,0.0);
+  // allocate stiffness matrix A, RHS vector b and solution vector x
+  typedef Dune::DynamicMatrix<ctype> StiffnessMatrixType;
+  StiffnessMatrixType A(numNodes,numNodes,0.0);
 
-    typedef Dune::DynamicVector<double> VectorType;
-    VectorType b(numNodes,0.0);
-    VectorType x(numNodes,0.0);
+  typedef Dune::DynamicVector<ctype> VectorType;
+  VectorType b(numNodes,0.0);
+  VectorType x(numNodes,0.0);
 
-    // basis functions
-    typedef std::function<double(double&&)> FunctionType;
+  // basis functions
+  typedef std::function<double(double&&)> FunctionType;
 
-    std::vector<FunctionType> phi(2);
-    phi[0]=phi0; // phi[0]=[](double& x)->double{return 1.0-x;};
-    phi[1]=phi1; // phi[1]=[](double& x)->double{return x;};
+  std::vector<FunctionType> phi(2);
+  phi[0]=phi0;
+  phi[1]=phi1;
 
-    std::vector<FunctionType> derphi(2);
-    derphi[0]=derphi0; // derphi[0]=[](double& x)->double{return -1.0;};
-    derphi[1]=derphi1; // derphi[1]=[](double& x)->double{return 1;};
+  std::vector<FunctionType> derphi(2);
+  derphi[0]=derphi0;
+  derphi[1]=derphi1;
 
-    // launch a group of threads to assemble the stiffness matrix and the RHS
-    std::vector<std::thread> t(numThreads);
+  // launch a group of threads to assemble the stiffness matrix and the RHS
+  std::vector<std::thread> t(numThreads);
 
-    for(size_t i=0;i!=numThreads;++i) t[i]=std::thread(assemble<StiffnessMatrixType,VectorType,ThreadsIndexType,GridType,std::vector<FunctionType>>,i,std::ref(A),std::ref(b),std::ref(tit),std::ref(grid),numGridElementsPerThread,std::ref(phi),std::ref(derphi));
-    for(size_t i=0;i!=numThreads;++i) t[i].join();
+  for(size_t i=0;i!=numThreads;++i) t[i]=std::thread(assemble<StiffnessMatrixType,VectorType,ThreadsIndexType,GridType,std::vector<FunctionType>>,i,std::ref(A),std::ref(b),std::ref(tit),std::ref(grid),numGridElementsPerThread,std::ref(phi),std::ref(derphi));
+  for(size_t i=0;i!=numThreads;++i) t[i].join();
 
-    // impose boundary condition
-    A[0][0]=1;
-    for(size_t i=1;i!=numNodes;++i) A[0][i]=0;
-    b[0]=ux0;
+  // impose boundary condition
+  A[0][0]=1;
+  for(size_t i=1;i!=numNodes;++i) A[0][i]=0;
+  b[0]=ux0;
 
-    A[numNodes-1][numNodes-1]=1;
-    for(size_t i=0;i!=(numNodes-1);++i) A[numNodes-1][i]=0;
-    b[numNodes-1]=ux1;
+  A[numNodes-1][numNodes-1]=1;
+  for(size_t i=0;i!=(numNodes-1);++i) A[numNodes-1][i]=0;
+  b[numNodes-1]=ux1;
 
-    #ifdef DEBUG_THREADS_TEST
-    #if DEBUG_THREADS_TEST
-    std::cout<<"DEBUG: global stiffness matrix"<<std::endl;
-    printMatrix(A);
-    std::cout<<"DEBUG: global RHS"<<std::endl;
-    printVector(b);
-    #endif
-    #endif
+  print("global stiffness matrix");
+  printMatrix(A);
+  print("global RHS");
+  printVector(b);
 
-    // solve the problem
-    A.solve(x,b);
+  // solve the problem
+  A.solve(x,b);
 
-    #ifdef DEBUG_THREADS_TEST
-    #if DEBUG_THREADS_TEST
-    std::cout<<"DEBUG: global solution"<<std::endl;
-    printVector(x);
-    #endif
-    #endif
+  print("global solution");
+  printVector(x);
 
-    // clean mutex
-    for(ThreadsIndexType::iterator it=tit.begin();it!=tit.end();++it){
-        if(it->first==shared) delete it->second;
-    }
+  // clean mutex
+  for(ThreadsIndexType::iterator it=tit.begin();it!=tit.end();++it){
+    if(it->first==shared) delete it->second;
+  }
 
-    return 0;
+  return 0;
 
 }
