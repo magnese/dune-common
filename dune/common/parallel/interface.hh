cebf4a24c504956c15a3147d77e9b377c13e853d
Marco Agnese <marco.agnese88@gmail.com>
2014-08-17 10:47:52 +0100

    interface: cleanup

--- dune/common/parallel/interface.hh
+++ dune/common/parallel/interface.hh
@@ -17,18 +17,15 @@ namespace Dune
    */
   /**
    * @file
-   * @brief Provides classes for building the communication
-   * interface between remote indices.
-   * @author Markus Blatt
+   * @brief Provides classes for building the communication interface between remote indices.
+   * @author Marco Agnese, Markus Blatt
    */
 
   /** @} */
   /**
-   * @brief Base class of all classes representing a communication
-   * interface.
+   * @brief Base class of all classes representing a communication interface.
    *
-   * It provides an generic utility method for building the interface
-   * for a set of remote indices.
+   * It provides an generic utility method for building the interface for a set of remote indices.
    */
   class InterfaceBuilder
   {
@@ -40,9 +37,7 @@ namespace Dune
     {}
 
   protected:
-    /**
-     * @brief Not for public use.
-     */
+    /** @brief Not for public use. */
     InterfaceBuilder()
     {}
 
@@ -50,64 +45,50 @@ namespace Dune
      * @brief Builds the interface between remote processes.
      *
      *
-     * The types T1 and T2 are classes representing a set of
-     * enumeration values of type InterfaceBuilder::Attribute. They have to provide
-     * a (static) method
+     * The types T1 and T2 are classes representing a set of enumeration values of type InterfaceBuilder::Attribute. They have to provide a (static) method
      * \code
      * bool contains(Attribute flag) const;
      * \endcode
-     * for checking whether the set contains a specfic flag.
-     * This functionality is for example provided the classes
-     * EnumItem, EnumRange and Combine.
+     * for checking whether the set contains a specfic flag. This functionality is for example provided the classes EnumItem, EnumRange and Combine.
      *
-     * If the template parameter send is true the sending side of
-     * the interface will be built, otherwise the information for
-     * receiving will be built.
+     * If the template parameter send is true the sending side of the interface will be built, otherwise the information for receiving will be built.
      *
      *
-     * If the template parameter send is true we create interface for sending
-     * in a forward communication.
+     * If the template parameter send is true we create interface for sending in a forward communication.
      *
      * @param remoteIndices The indices known to remote processes.
      * @param sourceFlags The set of flags marking source indices.
      * @param destFlags The setof flags markig destination indices.
-     * @param functor A functor for callbacks. It should provide the
-     * following methods:
+     * @param functor A functor for callbacks. It should provide the following methods:
      * \code
-     * // Reserve memory for the interface to processor proc. The interface
-     * // has to hold size entries
+     * // Reserve memory for the interface to processor proc. The interface has to hold size entries
      * void reserve(int proc, int size);
      *
-     * // Add an entry to the interface
-     * // We will send/receive size entries at index local to process proc
+     * // Add an entry to the interface. We will send/receive size entries at index local to process proc
      * void add(int proc, int local);
      * \endcode
      */
     template<class R, class T1, class T2, class Op, bool send>
-    void buildInterface (const R& remoteIndices,
-                         const T1& sourceFlags, const T2& destFlags,
-                         Op& functor) const;
+    void buildInterface (const R& remoteIndices, const T1& sourceFlags, const T2& destFlags, Op& functor) const;
   };
 
   /**
    * @brief Information describing an interface.
    *
-   * This class is used for temporary gathering information
-   * about the interface needed for actually building it. It
-   * is used be class Interface as functor for InterfaceBuilder::build.
+   * This class is used for temporary gathering information about the interface needed for actually building it.
+   * It is used be class Interface as functor for InterfaceBuilder::build.
    */
   class InterfaceInformation
   {
 
   public:
 
-    /**
-     * @brief Get the number of entries in the interface.
-     */
+    /** @brief Get the number of entries in the interface. */
     size_t size() const
     {
       return size_;
     }
+
     /**
      * @brief Get the local index for an entry.
      * @param i The  index of the entry.
@@ -117,6 +98,7 @@ namespace Dune
       assert(i<size_);
       return indices_[i];
     }
+
     /**
      * @brief Get the local index for an entry.
      * @param i The  index of the entry.
@@ -126,6 +108,7 @@ namespace Dune
       assert(i<size_);
       return indices_[i];
     }
+
     /**
      * @brief Reserve space for a number of entries.
      * @param size The maximum number of entries to hold.
@@ -134,11 +117,9 @@ namespace Dune
     {
       indices_ = new std::size_t[size];
       maxSize_ = size;
-
     }
-    /**
-     * brief Frees allocated memory.
-     */
+
+    /** brief Frees allocated memory. */
     void free()
     {
       if(indices_)
@@ -147,17 +128,15 @@ namespace Dune
       size_=0;
       indices_=0;
     }
-    /**
-     * @brief Add a new index to the interface.
-     */
+
+    /** @brief Add a new index to the interface. */
     void add(std::size_t index)
     {
       assert(size_<maxSize_);
       indices_[size_++]=index;
     }
 
-    InterfaceInformation()
-      : size_(0), maxSize_(0), indices_(0)
+    InterfaceInformation() : size_(0), maxSize_(0), indices_(0)
     {}
 
     virtual ~InterfaceInformation()
@@ -179,17 +158,13 @@ namespace Dune
     }
 
   private:
-    /**
-     * @brief The number of entries in the interface.
-     */
+    /** @brief The number of entries in the interface. */
     size_t size_;
-    /**
-     * @brief The maximum number of indices we can hold.
-     */
+
+    /** @brief The maximum number of indices we can hold. */
     size_t maxSize_;
-    /**
-     * @brief The local indices of the interface.
-     */
+
+    /** @brief The local indices of the interface. */
     std::size_t* indices_;
   };
 
@@ -201,70 +176,51 @@ namespace Dune
   /**
    * @brief Communication interface between remote and local indices.
    *
-   * Describes the communication interface between
-   * indices on the local process and those on remote processes.
+   * Describes the communication interface between indices on the local process and those on remote processes.
    */
   class Interface : public InterfaceBuilder
   {
 
   public:
-    /**
-     * @brief The type of the map form process number to InterfaceInformation for
-     * sending and receiving to and from it.
-     */
+    /** @brief The type of the map form process number to InterfaceInformation for sending and receiving to and from it. */
     typedef std::map<int,std::pair<InterfaceInformation,InterfaceInformation> > InformationMap;
 
     /**
      * @brief Builds the interface.
      *
-     * The types T1 and T2 are classes representing a set of
-     * enumeration values of type Interface::Attribute. They have to provide
-     * a (static) method
+     * The types T1 and T2 are classes representing a set of enumeration values of type Interface::Attribute. They have to provide a (static) method
      * \code
      * bool contains(Attribute flag) const;
      * \endcode
-     * for checking whether the set contains a specfic flag.
-     * This functionality is for example provided the classes
-     * EnumItem, EnumRange and Combine.
+     * for checking whether the set contains a specfic flag. This functionality is for example provided the classes EnumItem, EnumRange and Combine.
      * @param remoteIndices The indices known to remote processes.
      * @param sourceFlags The set of flags marking indices we send from.
      * @param destFlags The set of flags marking indices we receive for.
      */
     template<typename R, typename T1, typename T2>
-    void build(const R& remoteIndices, const T1& sourceFlags,
-               const T2& destFlags);
+    void build(const R& remoteIndices, const T1& sourceFlags, const T2& destFlags);
 
-    /**
-     * @brief Frees memory allocated during the build.
-     */
+    /** @brief Frees memory allocated during the build. */
     void free();
 
-    /**
-     * @brief Get the MPI Communicator.
-     */
+    /** @brief Get the MPI Communicator. */
     MPI_Comm communicator() const;
 
     /**
      * @brief Get information about the interfaces.
      *
      * @return Map of the interfaces.
-     * The key of the map is the process number and the value
-     * is the information pair (first the send and then the receive
-     * information).
+     * The key of the map is the process number and the value is the information pair (first the send and then the receive information).
      */
     const InformationMap& interfaces() const;
 
-    Interface(MPI_Comm comm)
-      : communicator_(comm), interfaces_()
+    Interface(MPI_Comm comm) : communicator_(comm), interfaces_()
     {}
 
-    Interface()
-      : communicator_(MPI_COMM_NULL), interfaces_()
+    Interface() : communicator_(MPI_COMM_NULL), interfaces_()
     {}
 
-    /**
-     * @brief Print the interface to std::out for debugging.
-     */
+    /** @brief Print the interface to std::out for debugging. */
     void print() const;
 
     bool operator!=(const Interface& o) const
@@ -293,9 +249,7 @@ namespace Dune
       return true;
     }
 
-    /**
-     * @brief Destructor.
-     */
+    /** @brief Destructor. */
     virtual ~Interface();
 
     void strip();
@@ -305,9 +259,7 @@ namespace Dune
      * @brief Get information about the interfaces.
      *
      * @return Map of the interfaces.
-     * The key of the map is the process number and the value
-     * is the information pair (first the send and then the receive
-     * information).
+     * The key of the map is the process number and the value is the information pair (first the send and then the receive information).
      */
     InformationMap& interfaces();
 
@@ -318,9 +270,7 @@ namespace Dune
     /**
      * @brief Information about the interfaces.
      *
-     * The key of the map is the process number and the value
-     * is the information pair (first the send and then the receive
-     * information).
+     * The key of the map is the process number and the value is the information pair (first the send and then the receive information).
      */
     InformationMap interfaces_;
 
@@ -328,8 +278,7 @@ namespace Dune
     class InformationBuilder
     {
     public:
-      InformationBuilder(InformationMap& interfaces)
-        : interfaces_(interfaces)
+      InformationBuilder(InformationMap& interfaces) : interfaces_(interfaces)
       {}
 
       void reserve(int proc, int size)
@@ -359,7 +308,7 @@ namespace Dune
 
     if(!remoteIndices.isSynced())
       DUNE_THROW(RemoteIndicesStateError,"RemoteIndices is not in sync with the index set. Call RemoteIndices::rebuild first!");
-    // Allocate the memory for the data type construction.
+    // allocate the memory for the data type construction
     typedef R RemoteIndices;
     typedef typename RemoteIndices::RemoteIndexMap::const_iterator const_iterator;
 
@@ -369,22 +318,21 @@ namespace Dune
 
     MPI_Comm_rank(remoteIndices.communicator(), &rank);
 
-    // Allocate memory for the type construction.
-    for(const_iterator process=remoteIndices.begin(); process != end; ++process) {
-      // Messure the number of indices send to the remote process first
+    // allocate memory for the type construction
+    for(const_iterator process=remoteIndices.begin(); process != end; ++process)
+    {
+      // messure the number of indices send to the remote process first
       int size=0;
       typedef typename RemoteIndices::RemoteIndexList::const_iterator RemoteIterator;
-      const RemoteIterator remoteEnd = send ? process->second.first->end() :
-                                       process->second.second->end();
+      const RemoteIterator remoteEnd = send ? process->second.first->end() : process->second.second->end();
       RemoteIterator remote = send ? process->second.first->begin() : process->second.second->begin();
 
-      while(remote!=remoteEnd) {
-        if( send ?  destFlags.contains(remote->attribute()) :
-            sourceFlags.contains(remote->attribute())) {
-
+      while(remote!=remoteEnd)
+      {
+        if( send ?  destFlags.contains(remote->attribute()) : sourceFlags.contains(remote->attribute()))
+        {
           // do we send the index?
-          if( send ? sourceFlags.contains(remote->localIndexPair().local().attribute()) :
-              destFlags.contains(remote->localIndexPair().local().attribute()))
+          if( send ? sourceFlags.contains(remote->localIndexPair().local().attribute()) : destFlags.contains(remote->localIndexPair().local().attribute()))
             ++size;
         }
         ++remote;
@@ -393,19 +341,18 @@ namespace Dune
     }
 
     // compare the local and remote indices and set up the types
-
-    for(const_iterator process=remoteIndices.begin(); process != end; ++process) {
+    for(const_iterator process=remoteIndices.begin(); process != end; ++process)
+    {
       typedef typename RemoteIndices::RemoteIndexList::const_iterator RemoteIterator;
-      const RemoteIterator remoteEnd = send ? process->second.first->end() :
-                                       process->second.second->end();
+      const RemoteIterator remoteEnd = send ? process->second.first->end() : process->second.second->end();
       RemoteIterator remote = send ? process->second.first->begin() : process->second.second->begin();
 
-      while(remote!=remoteEnd) {
-        if( send ?  destFlags.contains(remote->attribute()) :
-            sourceFlags.contains(remote->attribute())) {
+      while(remote!=remoteEnd)
+      {
+        if( send ?  destFlags.contains(remote->attribute()) : sourceFlags.contains(remote->attribute()))
+        {
           // do we send the index?
-          if( send ? sourceFlags.contains(remote->localIndexPair().local().attribute()) :
-              destFlags.contains(remote->localIndexPair().local().attribute()))
+          if( send ? sourceFlags.contains(remote->localIndexPair().local().attribute()) : destFlags.contains(remote->localIndexPair().local().attribute()))
             interfaceInformation.add(process->first,remote->localIndexPair().local().local());
         }
         ++remote;
@@ -418,7 +365,6 @@ namespace Dune
     return communicator_;
 
   }
-
 
   inline const std::map<int,std::pair<InterfaceInformation,InterfaceInformation> >& Interface::interfaces() const
   {
@@ -437,54 +383,54 @@ namespace Dune
     int rank;
     MPI_Comm_rank(communicator(), &rank);
 
-    for(const_iterator infoPair=interfaces_.begin(); infoPair!=end; ++infoPair) {
+    for(const_iterator infoPair=interfaces_.begin(); infoPair!=end; ++infoPair)
+    {
       {
         std::cout<<rank<<": send for process "<<infoPair->first<<": ";
         const InterfaceInformation& info(infoPair->second.first);
         for(size_t i=0; i < info.size(); i++)
           std::cout<<info[i]<<" ";
         std::cout<<std::endl;
-      } {
-
+      }
+      {
         std::cout<<rank<<": receive for process "<<infoPair->first<<": ";
         const InterfaceInformation& info(infoPair->second.second);
         for(size_t i=0; i < info.size(); i++)
           std::cout<<info[i]<<" ";
         std::cout<<std::endl;
       }
-
     }
   }
 
   template<typename R, typename T1, typename T2>
-  inline void Interface::build(const R& remoteIndices, const T1& sourceFlags,
-                               const T2& destFlags)
+  inline void Interface::build(const R& remoteIndices, const T1& sourceFlags, const T2& destFlags)
   {
     communicator_=remoteIndices.communicator();
 
     assert(interfaces_.empty());
 
-    // Build the send interface
+    // build the seind interface
     InformationBuilder<true> sendInformation(interfaces_);
-    this->template buildInterface<R,T1,T2,InformationBuilder<true>,true>(remoteIndices, sourceFlags,
-                                                                         destFlags, sendInformation);
+    this->template buildInterface<R,T1,T2,InformationBuilder<true>,true>(remoteIndices, sourceFlags, destFlags, sendInformation);
 
-    // Build the receive interface
+    // build the receive interface
     InformationBuilder<false> recvInformation(interfaces_);
-    this->template buildInterface<R,T1,T2,InformationBuilder<false>,false>(remoteIndices,sourceFlags,
-                                                                           destFlags, recvInformation);
+    this->template buildInterface<R,T1,T2,InformationBuilder<false>,false>(remoteIndices,sourceFlags, destFlags, recvInformation);
     strip();
   }
+
   inline void Interface::strip()
   {
     typedef InformationMap::iterator const_iterator;
     for(const_iterator interfacePair = interfaces_.begin(); interfacePair != interfaces_.end();)
-      if(interfacePair->second.first.size()==0 && interfacePair->second.second.size()==0) {
+      if(interfacePair->second.first.size()==0 && interfacePair->second.second.size()==0)
+      {
         interfacePair->second.first.free();
         interfacePair->second.second.free();
         const_iterator toerase=interfacePair++;
         interfaces_.erase(toerase);
-      }else
+      }
+      else
         ++interfacePair;
   }
 
@@ -493,7 +439,8 @@ namespace Dune
     typedef InformationMap::iterator iterator;
     typedef InformationMap::const_iterator const_iterator;
     const const_iterator end = interfaces_.end();
-    for(iterator interfacePair = interfaces_.begin(); interfacePair != end; ++interfacePair) {
+    for(iterator interfacePair = interfaces_.begin(); interfacePair != end; ++interfacePair)
+    {
       interfacePair->second.first.free();
       interfacePair->second.second.free();
     }
@@ -513,8 +460,8 @@ namespace std
   {
     typedef Dune::Interface::InformationMap InfoMap;
     typedef InfoMap::const_iterator Iter;
-    for(Iter i=interface.interfaces().begin(), end = interface.interfaces().end();
-        i!=end; ++i)
+    Iter end = interface.interfaces().end();
+    for(Iter i=interface.interfaces().begin(); i!=end; ++i)
     {
       os<<i->first<<": [ source=[";
       for(std::size_t j=0; j < i->second.first.size(); ++j)
