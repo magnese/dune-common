80c78dee66fe8c859468262ad058cd8109e2e5fa
Marco Agnese <marco.agnese88@gmail.com>
2015-05-17 18:27:09 +0100

    cleanup of paralleltest, removed test and fixed CMakeList.txt and Makefile.am

--- dune/common/parallel/test/paralleltest.cc
+++ dune/common/parallel/test/paralleltest.cc
@@ -16,33 +16,43 @@
 
 // policy: copy
 template<typename T>
-class CopyData{
-
+class CopyData
+{
 public:
 
   typedef typename T::value_type IndexedType;
 
-  static IndexedType gather(const T& v,int i){return v[i];}
-  static void scatter(T& v,IndexedType item,int i){v[i]=item;}
-
+  static IndexedType gather(const T& v,int i)
+  {
+    return v[i];
+  }
+  static void scatter(T& v,IndexedType item,int i)
+  {
+    v[i]=item;
+  }
 };
 
 // policy: add
 template<typename T>
-class AddData{
-
+class AddData
+{
 public:
 
   typedef typename T::value_type IndexedType;
 
-  static IndexedType gather(const T& v,int i){return v[i];}
-  static void scatter(T& v,IndexedType item,int i){v[i]+=item;}
-
+  static IndexedType gather(const T& v,int i)
+  {
+    return v[i];
+  }
+  static void scatter(T& v,IndexedType item,int i)
+  {
+    v[i]+=item;
+  }
 };
 
 
-int main(int argc,char** argv){
-
+int main(int argc,char** argv)
+{
   #if HAVE_MPI
 
   // init MPI
@@ -50,39 +60,47 @@ int main(int argc,char** argv){
   Dune::CollectiveCommunication<MPI_Comm> collCom(MPI_COMM_WORLD);
 
   // get size and rank
-  size_t size(collCom.size());
-  size_t rank(collCom.rank());
+  std::size_t size(collCom.size());
+  std::size_t rank(collCom.rank());
 
   // 2 processes
-  if(size>2){
-    if(rank==0) std::cout<<std::endl<<"WARNING: run me again with just 2 processes"<<std::endl;
+  if(size!=2)
+  {
+    if(rank==0)
+      std::cout<<"WARNING: run me again with just 2 processes"<<std::endl;
   }
-  else{
-
+  else
+  {
     // define parallel local index and parallel index set
     enum flags{owner,ghost};
     typedef Dune::ParallelLocalIndex<flags> LocalIndexType;
-    typedef Dune::ParallelIndexSet<size_t,LocalIndexType,7> ParallelIndexType;
+    typedef Dune::ParallelIndexSet<std::size_t,LocalIndexType,7> ParallelIndexType;
 
     // create parallel index set s
     ParallelIndexType sis;
-    size_t offsetGlobal(0);
-    size_t offsetLocal(0);
+    std::size_t offsetGlobal(0);
+    std::size_t offsetLocal(0);
 
-    if(rank==1){
+    if(rank==1)
+    {
       offsetGlobal=6;
       offsetLocal=1;
     }
 
     sis.beginResize();
-    for(size_t i=0;i!=6;++i) sis.add(i+offsetGlobal,LocalIndexType(i+offsetLocal,owner));
-    if(rank==0) sis.add(6,LocalIndexType(6,ghost));
-    else sis.add(5,LocalIndexType(0,ghost));
+    for(std::size_t i=0;i!=6;++i)
+      sis.add(i+offsetGlobal,LocalIndexType(i+offsetLocal,owner));
+    if(rank==0)
+      sis.add(6,LocalIndexType(6,ghost));
+    else
+      sis.add(5,LocalIndexType(0,ghost));
     sis.endResize();
 
     // output sis
-    for(size_t i=0;i!=size;++i){
-      if(rank==i){
+    for(std::size_t i=0;i!=size;++i)
+    {
+      if(rank==i)
+      {
         std::cout<<"s"<<rank<<":"<<std::endl;
         std::cout<<sis<<std::endl<<std::endl;
       }
@@ -103,37 +121,42 @@ int main(int argc,char** argv){
     infS.build(riS,ownerFlags,ghostFlags);
 
     // create local vector al
-    typedef int ctype;
-    typedef typename std::vector<ctype> VectorType;
+    typedef typename std::vector<int> VectorType;
     VectorType al(7,0);
-    typedef typename VectorType::iterator VectorItType;
 
     typedef typename ParallelIndexType::iterator PIndexIterType;
-    for(PIndexIterType it=sis.begin();it!=sis.end();++it){
-      if(it->local().attribute()==owner) al[it->local().local()]=(it-sis.begin())+5*rank;
-    }
+    for(PIndexIterType it=sis.begin();it!=sis.end();++it)
+      if(it->local().attribute()==owner)
+        al[it->local().local()]=(it-sis.begin())+5*rank;
 
     // output al
-    for(size_t i=0;i!=size;++i){
-      if(rank==i){
+    for(std::size_t i=0;i!=size;++i)
+    {
+      if(rank==i)
+      {
         std::cout<<"Local vector on process "<<rank<<": al={ ";
-        for(VectorItType it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
+        for(auto val:al)
+          std::cout<<val<<" ";
         std::cout<<"}"<<std::endl;
       }
       collCom.barrier();
     }
 
     // do something on al
-    if(rank==0) std::cout<<std::endl<<"Performing the operation al[i]+=10*(rank+1) for only the owned entries"<<std::endl<<std::endl;
-    for(PIndexIterType it=sis.begin();it!=sis.end();++it){
-      if(it->local().attribute()==owner) al[it->local().local()]+=10*(rank+1);
-    }
+    if(rank==0)
+      std::cout<<std::endl<<"Performing the operation al[i]+=10*(rank+1) for only the owned entries"<<std::endl<<std::endl;
+    for(PIndexIterType it=sis.begin();it!=sis.end();++it)
+      if(it->local().attribute()==owner)
+        al[it->local().local()]+=10*(rank+1);
 
     // output al before communication
-    for(size_t i=0;i!=size;++i){
-      if(rank==i){
+    for(std::size_t i=0;i!=size;++i)
+    {
+      if(rank==i)
+      {
         std::cout<<"Local vector on process "<<rank<<": al={ ";
-        for(VectorItType it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
+        for(auto val:al)
+          std::cout<<val<<" ";
         std::cout<<"}"<<std::endl;
       }
       collCom.barrier();
@@ -145,14 +168,18 @@ int main(int argc,char** argv){
     bComm.build(al,al,infS);
 
     // communicate
-    if(rank==0) std::cout<<std::endl<<"Forward communication"<<std::endl<<std::endl;
+    if(rank==0)
+      std::cout<<std::endl<<"Forward communication"<<std::endl<<std::endl;
     bComm.forward<CopyData<VectorType>>(al,al);
 
     // output al after communication
-    for(size_t i=0;i!=size;++i){
-      if(rank==i){
+    for(std::size_t i=0;i!=size;++i)
+    {
+      if(rank==i)
+      {
         std::cout<<"Local vector on process "<<rank<<": al={ ";
-        for(VectorItType it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
+        for(auto val:al)
+          std::cout<<val<<" ";
         std::cout<<"}"<<std::endl;
       }
       collCom.barrier();
