95b8cd826b1e92048abb004b9d5536699ff27849
Marco Agnese <marco.agnese88@gmail.com>
2015-05-19 00:11:34 +0100

    renaming and cleanup

--- dune/common/parallel/test/threadremoteindicestest.cc
+++ dune/common/parallel/test/threadremoteindicestest.cc
@@ -18,53 +18,67 @@
 
 // policy: copy
 template<typename T>
-class CopyData{
-
+class CopyData
+{
 public:
 
   typedef typename T::value_type IndexedType;
 
-  static IndexedType gather(const T& v,int i){return v[i];}
-  static void scatter(T& v,IndexedType item,int i){v[i]=item;}
-
+  static IndexedType gather(const T& v,int i)
+  {
+    return v[i];
+  }
+  static void scatter(T& v,IndexedType item,int i)
+  {
+    v[i]=item;
+  }
 };
 
 // policy: add
 template<typename T>
-class AddData{
-
+class AddData
+{
 public:
 
   typedef typename T::value_type IndexedType;
 
-  static IndexedType gather(const T& v,int i){return v[i];}
-  static void scatter(T& v,IndexedType item,int i){v[i]+=item;}
-
+  static IndexedType gather(const T& v,int i)
+  {
+    return v[i];
+  }
+  static void scatter(T& v,IndexedType item,int i)
+  {
+    v[i]+=item;
+  }
 };
 
 // function run by each thread
 template<class C>
-void exec(const C& comm, const size_t tid, std::mutex& osmutex){
+void exec(const C& comm, const std::size_t tid, std::mutex& osmutex){
 
   // define parallel local index and parallel index set
   enum flags{owner,ghost};
   typedef Dune::ParallelLocalIndex<flags> LocalIndexType;
-  typedef Dune::ParallelIndexSet<size_t,LocalIndexType,7> ParallelIndexType;
+  typedef Dune::ParallelIndexSet<std::size_t,LocalIndexType,7> ParallelIndexType;
 
   // create parallel index set s
   ParallelIndexType sis;
-  size_t offsetGlobal(0);
-  size_t offsetLocal(0);
+  std::size_t offsetGlobal(0);
+  std::size_t offsetLocal(0);
 
-  if(tid==1){
+  if(tid==1)
+  {
     offsetGlobal=6;
     offsetLocal=1;
   }
 
   sis.beginResize();
-  for(size_t i=0;i!=6;++i) sis.add(i+offsetGlobal,LocalIndexType(i+offsetLocal,owner));
-  if(tid==0) sis.add(6,LocalIndexType(6,ghost));
-  else sis.add(5,LocalIndexType(0,ghost));
+  for(std::size_t i=0;i!=6;++i)
+    sis.add(i+offsetGlobal,LocalIndexType(i+offsetLocal,owner));
+  if(tid==0)
+    sis.add(6,LocalIndexType(6,ghost));
+  else
+    sis.add(5,LocalIndexType(0,ghost));
   sis.endResize();
 
   // output sis
@@ -106,30 +120,32 @@ void exec(const C& comm, const size_t tid, std::mutex& osmutex){
   typedef int ctype;
   typedef typename std::vector<ctype> VectorType;
   VectorType al(7,0);
-  typedef typename VectorType::iterator VectorItType;
 
   typedef typename ParallelIndexType::iterator PIndexIterType;
-  for(PIndexIterType it=sis.begin();it!=sis.end();++it){
-    if(it->local().attribute()==owner) al[it->local().local()]=(it-sis.begin())+5*tid;
-  }
+  for(PIndexIterType it=sis.begin();it!=sis.end();++it)
+    if(it->local().attribute()==owner)
+      al[it->local().local()]=(it-sis.begin())+5*tid;
 
   // output al
   osmutex.lock();
   std::cout<<"Local vector on thread "<<tid<<": al={ ";
-  for(VectorItType it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
+  for(auto val:al)
+    std::cout<<val<<" ";
   std::cout<<"}"<<std::endl;
   osmutex.unlock();
 
   // do something on al
-  if(tid==0) std::cout<<std::endl<<"Performing the operation al[i]+=10*(tid+1) for only the owned entries"<<std::endl<<std::endl;
-  for(PIndexIterType it=sis.begin();it!=sis.end();++it){
-    if(it->local().attribute()==owner) al[it->local().local()]+=10*(tid+1);
-  }
+  if(tid==0)
+    std::cout<<std::endl<<"Performing the operation al[i]+=10*(tid+1) for only the owned entries"<<std::endl<<std::endl;
+  for(PIndexIterType it=sis.begin();it!=sis.end();++it)
+    if(it->local().attribute()==owner)
+      al[it->local().local()]+=10*(tid+1);
 
   // output al before communication
   osmutex.lock();
   std::cout<<"Local vector on thread "<<tid<<": al={ ";
-  for(VectorItType it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
+  for(auto val:al)
+    std::cout<<val<<" ";
   std::cout<<"}"<<std::endl;
   osmutex.unlock();
 
@@ -139,21 +155,23 @@ void exec(const C& comm, const size_t tid, std::mutex& osmutex){
   duneComm.template build<VectorType>(infS);
 
   // communicate
-  if(tid==0) std::cout<<std::endl<<"Forward communication"<<std::endl<<std::endl;
+  if(tid==0)
+    std::cout<<std::endl<<"Forward communication"<<std::endl<<std::endl;
   duneComm.template forward<CopyData<VectorType>>(al,al);
 
   // output al after communication
   osmutex.lock();
   std::cout<<"Local vector on thread "<<tid<<": al={ ";
-  for(VectorItType it=al.begin();it!=al.end();++it) std::cout<<*it<<" ";
+  for(auto val:al)
+    std::cout<<val<<" ";
   std::cout<<"}"<<std::endl;
   osmutex.unlock();
 }
 
-int main(int argc,char** argv){
-
+int main(int argc,char** argv)
+{
   // create thread communicator
-  const size_t numThreads(2);
+  const std::size_t numThreads(2);
   typedef Dune::ThreadCommunicator CommType;
   CommType comm(numThreads);
 
@@ -163,8 +181,10 @@ int main(int argc,char** argv){
   // launch a group of threads to run exec()
   std::vector<std::thread> t(numThreads);
 
-  for(size_t tid=0;tid!=numThreads;++tid) t[tid]=std::thread(exec<CommType>,std::ref(comm),tid,std::ref(osmutex));
-  for(size_t tid=0;tid!=numThreads;++tid) t[tid].join();
+  for(std::size_t tid=0;tid!=numThreads;++tid)
+    t[tid]=std::thread(exec<CommType>,std::ref(comm),tid,std::ref(osmutex));
+  for(std::size_t tid=0;tid!=numThreads;++tid)
+    t[tid].join();
 
   return 0;
 }
